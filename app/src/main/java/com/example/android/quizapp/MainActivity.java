5fc9f6c3f5f3a6103aa239e33b33142624f06300
Jon Leverkuhn <JonLeverkuhn@gmail.com>
2018-03-22 23:33:54 -0600

    Flesh out activity_main, create "easy" string array XML, and remove all the array code from MainActivity

--- app/src/main/java/com/example/android/quizapp/MainActivity.java
+++ app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -57,29 +57,12 @@ public class MainActivity extends AppCompatActivity {
         question = question + 1;
     }
 
-    public void buildQuestionArray(String difficulty) {
-        getResources().getString(R.string.app_name);
-        questionAry = new String[][] {
-                {""},
-                {"Easy Question 1","Type","Easy Question 1 Options","Easy Question 1 Answer","Image"},
-                {"Easy Question 2","Type","Easy Question 2 Options","Easy Question 2 Answer","Image"},
-                {"Easy Question 3","Type","Easy Question 3 Options","Easy Question 3 Answer","Image"},
-                {"Easy Question 4","Type","Easy Question 4 Options","Easy Question 4 Answer","Image"},
-                {"Easy Question 5","Type","Easy Question 5 Options","Easy Question 5 Answer","Image"}
-        };
-    }
-
-
     public void setEasyQuizParams(View view) {
         setUserInfo();
         if (!inputsReceived) {
             return;
         }
         setContentView(R.layout.activity_main);
-        difficulty="Easy";
-        buildQuestionArray(difficulty);
-        setQuestionDisplay();
-
     }
 
     public void setMediumQuizParams(View view) {
@@ -88,15 +71,6 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
         setContentView(R.layout.activity_main);
-        difficulty="Medium";
-        questionAry = new String[][] {
-                {""},
-                {"Medium Question 1","Type","Medium Question 1 Options","Medium Question 1 Answer","Image"},
-                {"Medium Question 2","Type","Medium Question 2 Options","Medium Question 2 Answer","Image"},
-                {"Medium Question 3","Type","Medium Question 3 Options","Medium Question 3 Answer","Image"},
-                {"Medium Question 4","Type","Medium Question 4 Options","Medium Question 4 Answer","Image"},
-                {"Medium Question 5","Type","Medium Question 5 Options","Medium Question 5 Answer","Image"}
-        };
     }
 
     public void setHardQuizParams(View view) {
@@ -105,14 +79,6 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
         setContentView(R.layout.activity_main);
-        difficulty="Hard";
-        questionAry = new String[][] {
-                {""},
-                {"Hard Question 1","Type","Hard Question 1 Options","Hard Question 1 Answer","Image"},
-                {"Hard Question 2","Type","Hard Question 2 Options","Hard Question 2 Answer","Image"},
-                {"Hard Question 3","Type","Hard Question 3 Options","Hard Question 3 Answer","Image"},
-                {"Hard Question 4","Type","Hard Question 4 Options","Hard Question 4 Answer","Image"},
-                {"Hard Question 5","Type","Hard Question 5 Options","Hard Question 5 Answer","Image"}
-        };
     }
+
 }
